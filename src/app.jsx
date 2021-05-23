d04613342eb4d08c2918d82f91ab5cea76ca5143
ByungyeonKim <viva_kby@naver.com>
2021-05-23 22:17:08 +0900

    로그인 및 로그아웃 기능 추가

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/login/login';
 import styles from './app.module.css';
+import Maker from './components/maker/maker';
 
 function App({ authService }) {
   return (
     <div className={styles.loginWrap}>
-      <Login authService={authService} />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Login authService={authService} />
+          </Route>
+          <Route>
+            <Maker path='/maker' authService={authService} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './login.module.css';
 
 const Login = ({ authService }) => {
+  const history = useHistory();
+  const goToMaker = (userId) => {
+    history.push({
+      pathname: '/maker',
+      state: { id: userId },
+    });
+  };
   const onLogin = (event) => {
     authService //
       .login(event.currentTarget.textContent)
-      .then(console.log('성공'));
+      .then((data) => goToMaker(data.user.uid));
   };
+
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      user && goToMaker(user.uid);
+    });
+  });
+
   return (
     <section className={styles.loginContainer}>
       <Header />
       <section>
         <p className={styles.description}>
-          ⚠️ 리액트로 만든 <em>개인 프로젝트입니다.</em> 사진은 <strong>서버에 저장</strong>되며
-          민감한 개인정보는 저장하지 마세요. ❌
+          ⚠️ 리액트로 만든 <em>개인 프로젝트입니다.</em> 사진은{' '}
+          <strong>서버에 저장</strong>되며 민감한 개인정보는 저장하지 마세요. ❌
         </p>
         <ul>
           <li className={styles.loginItem}>
