90546c21b709d3f96cb7bbcac329e97f10fb8ceb
ByungyeonKim <viva_kby@naver.com>
2021-05-23 22:14:20 +0900

    로그아웃 버튼 추가

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './header.module.css';
 
-const Header = (props) => (
+const Header = ({ onLogout }) => (
   <header className={styles.header}>
+    {onLogout && (
+      <button onClick={onLogout} className={styles.logout}>
+        Logout
+      </button>
+    )}
     <img className={styles.logo} src='/images/logo.png' alt='자격증 로고' />
     <h1>안녕하세요, CARD MAKER입니다! 🎉</h1>
   </header>
