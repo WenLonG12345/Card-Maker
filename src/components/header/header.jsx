aebdd6266672ab79ae7f89d8fd60dc4cb0802aa7
ByungyeonKim <viva_kby@naver.com>
2021-05-30 22:22:14 +0900

    Fix: header UI

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import styles from './header.module.css';
 
-const Header = ({ onLogout }) => (
-  <header className={styles.header}>
-    {onLogout && (
-      <button onClick={onLogout} className={styles.logout}>
-        Logout
-      </button>
-    )}
-    <img className={styles.logo} src='/images/logo.png' alt='자격증 로고' />
-    {!onLogout && <h1>안녕하세요, CARD MAKER입니다! 🎉</h1>}
-  </header>
-);
+const Header = ({ onLogout }) => {
+  const displayType = onLogout ? styles.onLogout : styles.onLogin;
+  const headingType = onLogout
+    ? 'Card Maker'
+    : '안녕하세요, CARD MAKER입니다! 🎉';
+  return (
+    <header className={`${styles.header} ${displayType}`}>
+      {!onLogout && (
+        <img className={styles.logo} src='/images/logo.png' alt='로고' />
+      )}
+      <h1 className={`${styles.title} ${displayType}`}>{headingType}</h1>
+      {onLogout && (
+        <button onClick={onLogout} className={styles.logout}>
+          로그아웃
+        </button>
+      )}
+    </header>
+  );
+};
 
 export default Header;
