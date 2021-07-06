83842fa88b8675af0c44c40dfa87bd34869ba764
ByungyeonKim <viva_kby@naver.com>
2021-07-06 14:55:25 +0900

    Add Card download handler

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,5 +1,7 @@
-import React, { memo } from 'react';
+import React, { memo, useRef } from 'react';
 import styles from './card.module.css';
+import { saveAs } from 'file-saver';
+import domtoimage from 'dom-to-image';
 
 const DEFAULT_MAKER = 'Card Maker';
 const DEFAULT_IMAGE = '/images/default_logo.png';
@@ -8,16 +10,31 @@ const Card = memo(({ card }) => {
   const { name, title, message, fileURL, theme, cardMaker } = card;
   const url = fileURL || DEFAULT_IMAGE;
   const maker = cardMaker || DEFAULT_MAKER;
+  const cardRef = useRef();
+  const onCaptureBtn = () => {
+    domtoimage.toBlob(cardRef.current).then((blob) => {
+      saveAs(blob, 'card_maker.png');
+    });
+  };
 
   return (
-    <li className={`${styles.card} ${getStyles(theme)}`}>
-      <img className={styles.avatar} src={url} alt='프로필' />
-      <div className={styles.info}>
-        <h1 className={styles.name}>{name}</h1>
-        <p className={styles.title}>{title}</p>
-        <p className={styles.message}>{message}</p>
-        <p className={styles.date}>{getMakerDate()}</p>
-        <p className={styles.maker}>{maker}</p>
+    <li className={styles.cardContainer}>
+      <div
+        ref={cardRef}
+        id='card'
+        className={`${styles.card} ${getStyles(theme)}`}
+      >
+        <img className={styles.avatar} src={url} alt='프로필' />
+        <div className={styles.info}>
+          <h1 className={styles.name}>{name}</h1>
+          <p className={styles.title}>{title}</p>
+          <p className={styles.message}>{message}</p>
+          <p className={styles.date}>{getMakerDate()}</p>
+          <p className={styles.maker}>{maker}</p>
+        </div>
+        <button className={styles.downBtn} onClick={onCaptureBtn}>
+          <i className='fas fa-download'></i>
+        </button>
       </div>
     </li>
   );
