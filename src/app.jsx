ecd95cb4fbc43c13e953b167ab18ad2c64afee60
ByungyeonKim <viva_kby@naver.com>
2021-07-10 00:09:31 +0900

    Add particles-bg

--- src/app.jsx
+++ src/app.jsx
@@ -3,25 +3,32 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/login/login';
 import styles from './app.module.css';
 import Maker from './components/maker/maker';
+import ParticlesBg from 'particles-bg';
 
 function App({ FileInput, authService, cardRepository }) {
   return (
-    <div className={styles.loginWrap}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/'>
-            <Login authService={authService} />
-          </Route>
-          <Route path='/maker'>
-            <Maker
-              FileInput={FileInput}
-              authService={authService}
-              cardRepository={cardRepository}
-            />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <>
+      <div className={styles.loginWrap}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/'>
+              <ParticlesBg
+                type='polygon'
+                bg={{ position: 'absolute', zIndex: 0, top: 0, left: 0 }}
+              />
+              <Login authService={authService} />
+            </Route>
+            <Route path='/maker'>
+              <Maker
+                FileInput={FileInput}
+                authService={authService}
+                cardRepository={cardRepository}
+              />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
