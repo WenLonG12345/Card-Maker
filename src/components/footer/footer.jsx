ef2aa1dc5d3d6ec7c3f61c461a9644fb146e4546
ByungyeonKim <viva_kby@naver.com>
2021-07-16 23:21:06 +0900

    Add Github icon

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,7 +3,18 @@ import styles from './footer.module.css';
 
 const Footer = memo(() => (
   <footer className={styles.footer}>
-    <h1>친구, 지인의 카드를 만들어 보세요!</h1>
+    <h1>
+      친구, 지인의 카드를 만들어 보세요!{' '}
+      <a
+        className={styles.link}
+        title='BYTube GitHub'
+        href='https://github.com/ByungyeonKim'
+        rel='noopener noreferrer'
+        target='_blank'
+      >
+        <i className='fab fa-github'></i>
+      </a>
+    </h1>
   </footer>
 ));
 export default Footer;
